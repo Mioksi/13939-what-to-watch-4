0f351ef1b06d5807b87bd725a62c25a9dd2abdc5
Mioksi <mio.natali@hotmail.com>
2020-06-12 14:26:33 +0300

    Устанавливает prop-types и добавляет валидацию пропсов

--- src/common/consts.js
+++ src/common/consts.js
@@ -3,3 +3,26 @@ export const Movie = {
   GENRE: `Drama`,
   YEAR: 2014,
 };
+
+export const MOVIE_TITLES = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`,
+  `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`,
+  `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+  `Midnight Special`
+];
