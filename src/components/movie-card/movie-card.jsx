4af3fd3e3aac79e0ed20ac71cf36e9ffb6694e73
Mioksi <mio.natali@hotmail.com>
2020-06-18 15:54:35 +0300

    Добавляет компонент MovieCard

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+const MovieCard = ({movie, onCardTitleClick, onCardMouseEnter, onCardMouseLeave}) => {
+  const {id, title, image} = movie;
+
+  return (
+    <article
+      className="small-movie-card catalog__movies-card"
+      onMouseEnter={() => {
+        onCardMouseEnter(id);
+      }}
+      onMouseLeave={onCardMouseLeave}
+    >
+      <div className="small-movie-card__image">
+        <img src={image} alt={title} width="280" height="175"/>
+      </div>
+      <h3 className="small-movie-card__title">
+        <a onClick={onCardTitleClick} className="small-movie-card__link" href="movie-page.html">{title}</a>
+      </h3>
+    </article>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  onCardTitleClick: PropTypes.func.isRequired,
+  onCardMouseEnter: PropTypes.func.isRequired,
+  onCardMouseLeave: PropTypes.func.isRequired,
+};
+
+export default MovieCard;
