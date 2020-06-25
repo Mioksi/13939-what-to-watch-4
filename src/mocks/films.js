3de907691d2c6b990dc2912014aab6953c626959
Mioksi <mio.natali@hotmail.com>
2020-06-25 11:17:12 +0300

    Добавляет компонент VideoPlayer и снепшот тест

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -3,40 +3,48 @@ export const movies = [
     id: 1,
     title: `Fantastic Beasts: The Crimes of Grindelwald`,
     image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
   },
   {
     id: 2,
     title: `Bohemian Rhapsody`,
     image: `img/bohemian-rhapsody.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: 3,
     title: `Macbeth`,
     image: `img/macbeth.jpg`,
+    preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
   },
   {
     id: 4,
     title: `Aviator`,
     image: `img/aviator.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: 5,
     title: `We need to talk about Kevin`,
     image: `img/we-need-to-talk-about-kevin.jpg`,
+    preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
   },
   {
     id: 6,
     title: `What We Do in the Shadows`,
     image: `img/what-we-do-in-the-shadows.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: 7,
     title: `Revenant`,
     image: `img/revenant.jpg`,
+    preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
   },
   {
     id: 8,
     title: `Johnny English`,
     image: `img/johnny-english.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   }
 ];
