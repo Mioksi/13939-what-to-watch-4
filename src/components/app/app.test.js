84533b7166c94c52863476e2b95a99b12c108918
Mioksi <mio.natali@hotmail.com>
2020-06-27 21:01:48 +0300

    Обновляет существующие тесты

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -7,6 +7,7 @@ import {Movie, MOVIES} from '../../common/consts';
 const film = {
   title: `The Grand Budapest Hotel`,
   genre: `Drama`,
+  runTime: `1h 39m`,
   year: 2014,
   backgroundPoster: `img/bg-the-grand-budapest-hotel.jpg`,
   filmPoster: `img/the-grand-budapest-hotel-poster.jpg`,
@@ -17,6 +18,23 @@ const film = {
   starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other`
 };
 
+const reviews = [
+  {
+    id: 1,
+    author: `Kate Muir`,
+    date: `December 24, 2016`,
+    rating: 8.9,
+    text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years`,
+  },
+  {
+    id: 2,
+    author: `Bill Goodykoontz`,
+    date: `November 18, 2015`,
+    rating: 8.0,
+    text: `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+  },
+];
+
 it(`Render App`, () => {
   const tree = renderer
     .create(<App
@@ -25,6 +43,7 @@ it(`Render App`, () => {
       movieYear={Movie.YEAR}
       movies={MOVIES}
       film={film}
+      reviews={reviews}
     />, {
       createNodeMock: () => {
         return {};
