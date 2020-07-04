83baba68cfe1ce105052c4a977dcbc2108b31fbb
Mioksi <mio.natali@hotmail.com>
2020-07-04 12:38:48 +0300

    Исправляет замечания наставника

--- src/components/genres-list/genres-list.jsx
+++ src/components/genres-list/genres-list.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {MAX_GENRES} from '../../common/consts';
 
-const GenresList = (props) => {
-  const {allGenres, activeGenre, onGenreClick} = props;
+const setActiveClass = (activeGenre, genre) => {
+  return activeGenre === genre ? `catalog__genres-item--active` : ``;
+};
 
-  const setActiveClass = (genre) => {
-    return activeGenre === genre ? `catalog__genres-item--active` : ``;
-  };
+const getGenre = (genre, i, activeGenre, onGenreClick) => {
+  const genreClass = `catalog__genres-item ${setActiveClass(activeGenre, genre)}`;
+  const key = `${genre} + ${i}`;
 
-  const getGenre = (genre, i) => {
-    const genreClass = `catalog__genres-item ${setActiveClass(genre)}`;
-    const key = `${genre} + ${i}`;
-
-    return (
-      <li
-        className={genreClass}
-        key={key}
-      >
-        <a onClick={handleGenreClick(genre)} href="#" className="catalog__genres-link">{genre}</a>
-      </li>
-    );
-  };
-
-  const renderGenres = () => allGenres.slice(0, MAX_GENRES).map(getGenre);
-
-  const handleGenreClick = (genre) => {
+  const handleGenreClick = () => {
     return (evt) => {
       evt.preventDefault();
       onGenreClick(genre);
     };
+  };
+
+  return (
+    <li
+      className={genreClass}
+      key={key}
+    >
+      <a onClick={handleGenreClick(genre)} href="#" className="catalog__genres-link">{genre}</a>
+    </li>
+  );
+};
+
+const GenresList = ({allGenres, activeGenre, onGenreClick}) => {
+  const renderGenres = () => {
+    return allGenres.slice(0, MAX_GENRES).map((genre, i) => getGenre(genre, i, activeGenre, onGenreClick));
   };
 
   return (
