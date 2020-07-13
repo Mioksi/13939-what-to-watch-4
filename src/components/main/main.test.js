744506d1cba0db6e26ba9fcf96f8e08efda59b9e
Mioksi <mio.natali@hotmail.com>
2020-07-13 22:25:19 +0300

    Обновляет существующие тесты

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -12,6 +12,8 @@ const film = {
   title: `The Grand Budapest Hotel`,
   genre: `Drama`,
   year: 2014,
+  backgroundPoster: `img/bg-the-grand-budapest-hotel.jpg`,
+  src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
 };
 
 const mock = {
@@ -34,6 +36,8 @@ it(`Should Main render correctly`, () => {
             film={film}
             movies={MOVIES}
             shownMoviesCount={8}
+            isPlayerActive={false}
+            setFullscreenPlayer={() => {}}
             onCardClick={() => {}}
             onCardTitleClick={() => {}}
             onShowMoreButtonClick={() => {}}
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -8,6 +8,7 @@ const film = {
   year: 2014,
   backgroundPoster: `img/bg-the-grand-budapest-hotel.jpg`,
   filmPoster: `img/the-grand-budapest-hotel-poster.jpg`,
+  src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   rating: 8.9,
   ratingCount: 240,
   description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
@@ -134,6 +135,7 @@ describe(`Reducer work correctly`, () => {
       reviews,
       genresList: getGenresList(movies),
       shownMoviesCount: MAX_MOVIES,
+      isPlayerActive: false,
     });
   });
 
