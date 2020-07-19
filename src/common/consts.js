b20ca48c4cd9501ae07aba3ffed49b4617729a3d
Mioksi <mio.natali@hotmail.com>
2020-07-19 22:50:22 +0300

    Добавляет работу с API, удаляет моки

--- src/common/consts.js
+++ src/common/consts.js
@@ -2,6 +2,7 @@ export const VIDEO_DELAY = 1000;
 export const MAX_SIMILAR_MOVIES = 4;
 export const MAX_GENRES = 10;
 export const MAX_MOVIES = 8;
+export const TIMEOUT = 5000;
 
 export const MOVIES = [
   {
@@ -40,6 +41,12 @@ export const TabType = {
   REVIEWS: `Reviews`
 };
 
+export const Error = {
+  UNAUTHORIZED: 401
+};
+
 export const TABS = Object.values(TabType);
 export const ALL_GENRES = `All genres`;
 export const VIDEO_CLASS = `player__video`;
+
+export const URL = `https://4.react.pages.academy/wtw`;
